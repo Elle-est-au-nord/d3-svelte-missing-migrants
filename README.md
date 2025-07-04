c3d76601e2f6fb8cf6ab50fc6e38ba20634b1e81
Éléonore Mayola <eleonore.mayola@gmail.com>
2025-07-04 19:10:09 +0200

    Add Netlify badge to Readme, add data source to page

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # D3+Svelte: Visualizing Missing Migrants
 
 Using SvelteKit to create a single page app with D3 charts using the International Organization for Migration dataset on missing migrants.
+
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/f2e79d03-9a41-47cc-ac84-2f606cb0614e/deploy-status)](https://app.netlify.com/projects/vizmissingmigrants/deploys)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,17 @@
     (acc, curr) => acc + curr.victims, 0,
   );
   let formattedTotal = format(",")(totalVictims)
+  let { updated, source, link} = $state(data.iom.metadata)
 
 </script>
 
 <div class="@container">
-  <div class="flex flex-col gap-2 p-8 md:flex-row">
+  <div class="flex flex-col gap-2 p-8">
     <h1 class="text-3xl font-semibold underline text-slate-700/80">
       Visualizing Missing Migrants
     </h1>
+    <h4 class="text-slate-700/80">Project by <a class="text-sky-700/90 hover:text-green-700/80" href="https://elle-est-au-nord.com/" target="_blank">Eleonore M.</a> © 2025</h4>
+    <p class="text-xs text-slate-700/80">Data source: <span class="text-sky-700/90"><a href="{link}" target="_blank">{source}</a></span>, data updated in {updated}</p>
   </div>
   <div class="flex flex-col md:flex-row p-6 pb-0 bg-slate-400/40">
     <h2 class="text-2xl font-bold text-slate-800/90">According to IOM's records, <span class="text-rose-900/90">{formattedTotal}</span> people have died or gone missing on their migration routes</h2>
